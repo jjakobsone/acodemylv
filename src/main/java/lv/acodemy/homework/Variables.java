7f7877b09d1fd6bdacaf991b9f81b3b0d1219865
jjakobsone <jakobsone.julija@gmail.com>
2022-11-08 18:46:22 +0200

    [1_homework]: Add homework

---
+++ src/main/java/lv/acodemy/homework/Variables.java
@@ -0,0 +1,320 @@
+package lv.acodemy.homework;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        //Calorie in fruits
+        System.out.println("Calorie in fruits in 100gr:");
+
+        byte inzhir = 75;
+        System.out.println("Inzhir- "+ inzhir);
+
+        byte vinograd = 65;
+        System.out.println("Vinograd- " + vinograd);
+
+        byte hurma = 67;
+        System.out.println("Hurma- " + hurma);
+
+        byte chernika = 85;
+        System.out.println("Chernika- " + chernika);
+
+        byte greipfrut = 42;
+        System.out.println("Greipfrut- " + greipfrut);
+
+        byte golubika = 57;
+        System.out.println("Golubika- " + golubika);
+
+        byte chereshnja = 52;
+        System.out.println("Chereshnja- " + chereshnja);
+
+        byte klubnika = 32;
+        System.out.println("Klubnika- " + klubnika);
+
+        byte slivi = 46;
+        System.out.println("Slivi- " + slivi);
+
+        byte jabloko = 52;
+        System.out.println("Jabloko- " + jabloko);
+
+
+        //enter
+        System.out.println(" ");
+
+
+        // Rating of the most delicious fruits
+        System.out.println("Rating of the most delicious fruits:");
+
+        short mango = 6538;
+        System.out.println("Mango- " + mango);
+
+        short lichi = 6161;
+        System.out.println("Lichi- " + lichi);
+
+        short mangustin = 5863;
+        System.out.println("Mangustin- " + mangustin);
+
+        short glazDrakona = 4907;
+        System.out.println("Glaz Drakona- " + glazDrakona);
+
+        short rambutan = 4723;
+        System.out.println("Rambutan- " + rambutan);
+
+        short longan = 2845;
+        System.out.println("Longan- " + longan);
+
+        short banan = 2727;
+        System.out.println("Banan- " + banan);
+
+        short avokado = 2723;
+        System.out.println("Avokado- " + avokado);
+
+        short ananas = 2528;
+        System.out.println("Ananas - " + ananas);
+
+        short durian = 2441;
+        System.out.println("Durian-" + durian);
+
+
+        //enter
+        System.out.println(" ");
+
+
+        //Bukvi i ih porjadkovij nomer
+        System.out.println("Bukvi ih ih porjadkovij nomer:");
+
+        char a = 'A' + 1;
+        System.out.println("A- " + 1);
+
+        char b = 'B' + 2;
+        System.out.println("B- "+ 2);
+
+        char c = 'C' + 3;
+        System.out.println("C- " + 3);
+
+        char d = 'D' + 4;
+        System.out.println("D- " + 4);
+
+        char e = 'E' + 5;
+        System.out.println("E- " + 5);
+
+        char f = 'F' + 6;
+        System.out.println("F- " + 6);
+
+        char g = 'G' + 7;
+        System.out.println("G- " + 7);
+
+        char h = 'H' + 8;
+        System.out.println("H- " + 8);
+
+        char i = 'I' + 9;
+        System.out.println("I- " + 9);
+
+        char j = 'J' + 10;
+        System.out.println("J- " + 10);
+
+
+        //enter
+        System.out.println(" ");
+
+
+        //Population of Latvia
+        System.out.println("Population of Latvia:");
+
+        int year1992 = 2662883;
+        System.out.println("In 1992- " + year1992);
+
+        int year1994 = 2606454;
+        System.out.println("In 1994- " + year1994);
+
+        int year2000 = 2392530;
+        System.out.println("In 2000- " + year2000);
+
+        int year2004 = 2263443;
+        System.out.println("In 2004- " + year2004);
+
+        int year2009 = 2126265;
+        System.out.println("In 2009- " + year2009);
+
+        int year2012 = 2053636;
+        System.out.println("In 2012- " + year2012);
+
+        int year2016 = 1973476;
+        System.out.println("In 2016- " + year2016);
+
+        int year2018 = 1935630;
+        System.out.println("In 2018- " + year2018);
+
+        int year2020 = 1897052;
+        System.out.println("In 2020- " + year2020);
+
+        int year2022 = 1850651;
+        System.out.println("In 2022- " + year2022);
+
+
+        System.out.println(" ");
+
+
+        //Countries in the world by population in 2020
+        System.out.println("Countries in the world by population in 2020:");
+
+        long china = 1439323776L;
+        System.out.println("China- " + china);
+
+        long india = 1380004385L;
+        System.out.println("India- " + india);
+
+        long unitedStates = 331002651L;
+        System.out.println("United States- " + unitedStates);
+
+        long indonesia = 273523615L;
+        System.out.println("Indonesia- " + indonesia);
+
+        long pakistan = 220892340L;
+        System.out.println("Pakistan- " + pakistan);
+
+        long brasil = 212559417L;
+        System.out.println("Brasil- " + brasil);
+
+        long nigeria = 206139589L;
+        System.out.println("Nigeria- " + nigeria);
+
+        long bangladesh = 164689383L;
+        System.out.println("Bangladesh- " + bangladesh);
+
+        long russia = 145934462L;
+        System.out.println("Russia- " + russia);
+
+        long mexico = 128932753L;
+        System.out.println("Mexico- " + mexico);
+
+
+        System.out.println(" ");
+
+
+        //Gold Price in EUR per Gram for Last 10 Years
+        System.out.println("Gold Price in EUR per Gram for Last 10 Years:");
+
+        float feb2013 = 39.3694f;
+        System.out.println("In Feb 2013- " + feb2013);
+
+        float june2013 = 29.6394f;
+        System.out.println("In June 2013- " + june2013);
+
+        float march2014 = 31.3766f;
+        System.out.println("In March 2014- " + march2014);
+
+        float january2015 = 36.9398f;
+        System.out.println("In January 2015- " + january2015);
+
+        float march2017 = 37.0312f;
+        System.out.println("In March 2017- " + march2017);
+
+        float september2018 = 32.9568f;
+        System.out.println("In September 2018- " + september2018);
+
+        float september2019 = 44.3931f;
+        System.out.println("In September 2019- " + september2019);
+
+        float august2020 = 55.4713f;
+        System.out.println("In August 2020- " + august2020);
+
+        float july2021 = 48.1322f;
+        System.out.println("In July 2021- " + july2021);
+
+        float march2022 = 60.1290f;
+        System.out.println("In March 2022- " + march2022);
+
+
+        System.out.println(" ");
+
+
+        //Atomnaja massa himicheskih elementov
+        System.out.println("Atomnaja massa himicheskih elementov:");
+
+        double marganec =  54.938045;
+        System.out.println("Marganec- " + marganec);
+
+        double ittrij = 88.90585;
+        System.out.println("Ittrij- " + ittrij);
+
+        double niobij = 92.90638;
+        System.out.println("Niobij- " + niobij);
+
+        double rodij = 102.90550;
+        System.out.println("Rodij- " + rodij);
+
+        double serebro = 107.8682;
+        System.out.println("Serebro- " + serebro);
+
+        double iod = 126.9044;
+        System.out.println("Iod- " + iod);
+
+        double cezij = 132.9054519;
+        System.out.println("Cezij- " + cezij);
+
+        double lantan = 138.90547;
+        System.out.println("Lantan- " + lantan);
+
+        double golmij = 164.93032;
+        System.out.println("Golmij- " + golmij);
+
+        double zoloto = 196.966569;
+        System.out.println("Zoloto- " + zoloto);
+
+
+        System.out.println(" ");
+
+
+        //boolean
+
+        boolean todayIsNovember = true;
+        System.out.println("Today is November - " + todayIsNovember);
+
+        /*
+        Madonna's real name is Madonna - True - Madonna Louise Ciccone in full
+        An octopus has three hearts - True - an octopus has one main, systemic heart to pumps blood around its body. The two additional hearts are responsible for pumping blood over each of its gills.
+        Coffee is made from berries - True - When coffee berries turn from green to bright red in colour, this indicaties ripeness and they are picked, processed, and dried, before being roasted and turned into coffee.
+        The black box in a plane is black - False - they're actually orange
+        The unicorn is the national animal of Scotland - True
+        Alaska is the biggest American state in square miles - True - It spans a total area of 665,384 square miles. Texas comes in next with a total area of 268,596 square miles.
+        Prince Harry is taller than Prince William - False - Prince William is 1.91m, Prince Harry is 1.86m
+        Meghan Markle's first name is Rachel - True
+        Goldfish have a two second memory - False - Their memories can actually last for months
+         */
+
+        boolean madonnasRealNameIsMadonna = true;
+        System.out.println("Madonna's real name is Madonna- " + madonnasRealNameIsMadonna);
+
+        boolean anOctopusHasThreeHearts = true;
+        System.out.println("An octopus has three hearts- " + anOctopusHasThreeHearts);
+
+        boolean coffeeIsMadeFromBerries = true;
+        System.out.println("Coffee is made from berries- " + coffeeIsMadeFromBerries);
+
+        boolean blackBoxIsBlack = false;
+        System.out.println("The black box in a plane is black- " + blackBoxIsBlack);
+
+        boolean theUnicornIsTheNationalAnimalOfScotland = true;
+        System.out.println("The unicorn is the national animal of Scotland- " + theUnicornIsTheNationalAnimalOfScotland);
+
+        boolean alaskaIsTheBiggestAmericanStateInSquareMiles = true;
+        System.out.println("Alaska is the biggest American state in square miles- " + alaskaIsTheBiggestAmericanStateInSquareMiles);
+
+        boolean princeHarryIsTallerThanPrinceWilliam = false;
+        System.out.println("Prince Harry is taller than Prince William- " + princeHarryIsTallerThanPrinceWilliam);
+
+        boolean meghanMarklesFirstNameIsRachel = true;
+        System.out.println("Meghan Markle's first name is Rachel- " + meghanMarklesFirstNameIsRachel);
+
+        boolean goldfishHaveATwoSecondMemory = false;
+        System.out.println("Goldfish have a two second memory- " + goldfishHaveATwoSecondMemory);
+
+
+
+
+
+
+
+
+    }
+}
